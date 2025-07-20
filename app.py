1e1950ab4c591d1305a1534711a9b89c8b0206fc
dhruvjai45 <dhruvjain.gdsc@gmail.com>
2025-07-20 16:10:03 +0530

    requremenent.txt changes

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ CLASS_NAMES = [
 # — Load models once —
 @st.cache_resource
 def load_models():
-    driver_m = load_model("driver_behaviour.keras", compile=False)
-    drow_m = load_model("final_drowsiness_model.keras", compile=False)
-    steer_m = load_model("advanced_final_steering_model.keras", compile=False)
+    driver_m = load_model("models/driver_behaviour.keras")
+    drow_m = load_model("models/final_drowsiness_model.keras")
+    steer_m = load_model("models/advanced_final_steering_model.keras")
     return driver_m, drow_m, steer_m
 
 driver_model, drowsiness_model, steering_model = load_models()
